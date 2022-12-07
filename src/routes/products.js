c4600bf9645bc3399d52ebfb307c6b76e3f9baaa
vidyashree-r <80469310+vidyashree-r@users.noreply.github.com>
2022-12-07 15:27:46 +0100

    updated products

--- src/routes/products.js
+++ src/routes/products.js
@@ -96,8 +96,14 @@ router.delete("/sellProduct", async (req, res) => {
               if (articalElement.art_id.match(pr.art_id)) {
                 const stock = parseInt(articalElement.stock);
                 const amountOf = parseInt(pr.amount_of);
+                console.log("1", stock);
+                console.log("2", amountOf);
+                console.log("3", pr.stock);
+                if (parseInt(pr.stock) < amountOf) {
+                  console.log(`stock======================`, stock);
+                  // delete product
+                  finalProducts.products.splice(matchedProductIndex, 1);
 
-                if (stock === 1) {
                   // delete inventory
                   finalInventory = {
                     inventory: inventories.inventory.splice(
@@ -119,12 +125,15 @@ router.delete("/sellProduct", async (req, res) => {
                   };
                   finalInv.push(inv);
                   invIds.push(inv.art_id);
+                } else {
+                  console.log(`Stock is insufficient`);
+                  res.send("Stock is insufficient");
                 }
               }
             });
           });
-          // delete product
-          finalProducts.products.splice(matchedProductIndex, 1);
+          // // delete product
+          // finalProducts.products.splice(matchedProductIndex, 1);
 
           // Update inventories list
           const remainingInventory = inventories.inventory.filter(
@@ -141,8 +150,8 @@ router.delete("/sellProduct", async (req, res) => {
     console.log(`inventory:=======> ${JSON.stringify(finalInventory)}`);
     console.log(`products:=======> ${JSON.stringify(finalProduct)}`);
     res.send(`${JSON.stringify(finalProduct)}`);
-    updateData("src/assets/products.json", JSON.stringify(finalProduct));
-    updateData("src/assets/inventory.json", JSON.stringify(finalInventory));
+    // updateData("src/assets/products.json", JSON.stringify(finalProduct));
+    // updateData("src/assets/inventory.json", JSON.stringify(finalInventory));
   } catch (error) {
     console.log(`Error: ${error}`);
   }
